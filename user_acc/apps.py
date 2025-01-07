d2b6074aa513d04999e6945dec8c72985faf3624
gabrieljumanne <gabrieljumanne098@gmail.com>
2025-01-07 23:38:55 +0300

    registering signal.py in user_acc/apps.py

--- user_acc/apps.py
+++ user_acc/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_acc'
+
+    def ready(self):
+        import user_acc.signal
